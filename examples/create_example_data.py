c2aae96a27651ce8a41381240ed460b07ba1cafc
Josh <jsmith07@uw.edu>
2018-05-22 12:31:00 -0700

    Create example data to build simple PAMM procedure in python.

---
+++ examples/create_example_data.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+from sklearn.datasets import make_classification
+
+N = 900
+
+# Generate data set
+X1, true_class = make_classification(n_samples=N, n_features=2,
+                                     n_redundant=0, n_informative=2,
+                                     n_clusters_per_class=1, n_classes=3)
+
+# move data to DataFrame for nice output
+X_out = pd.DataFrame(X1)
+X_out.columns = ['x1', 'x2']
+X_out['class'] = true_class
+
+# save example data
+X_out.to_csv('example_data.csv')
