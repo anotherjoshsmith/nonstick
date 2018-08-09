3b00f760dadc19193794befeeb5240761a2b1a3e
Josh <jsmith07@uw.edu>
2018-08-08 17:22:11 -0700

    Setup tests directory for pytest.

---
+++ nonstick/tests/test_pamm.py
@@ -0,0 +1,25 @@
+import os
+import pytest
+
+import numpy as np
+
+import nonstick
+
+
+data_path = os.path.join(nonstick.__path__[0], 'data')
+
+
+class TestFarthestPointGrid(object):
+    pass
+
+
+class TestDensityEstimation(object):
+    pass
+
+
+class TestQuickShift(object):
+    pass
+
+
+class TestGaussianMixtureModel(object):
+    pass
