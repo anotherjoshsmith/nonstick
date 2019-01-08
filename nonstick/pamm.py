0671d816d71b2d70f7d55d85e8db19529a31e88e
Josh <jsmith07@uw.edu>
2019-01-07 16:10:50 -0800

    reformat with black

--- nonstick/pamm.py
+++ nonstick/pamm.py
@@ -11,28 +11,27 @@ from scipy.stats import multivariate_normal
 
 def main():
     np.random.seed(7)
-    data_dir = op.join(op.dirname(__file__), '../examples')
-    data_file = op.join(data_dir, 'example_data.csv')
+    data_dir = op.join(op.dirname(__file__), "../examples")
+    data_file = op.join(data_dir, "example_data.csv")
     # read example data
     data = pd.read_csv(data_file, index_col=0)
     X = data.values[:, :2]
+    y = data.values[:, 2]
 
     # scale data
     scaler = StandardScaler()
     X = scaler.fit_transform(X)
-    X_train, X_test = train_test_split(X)
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
 
     # get sample grid for KDE with farthest point algorithm
     N = X.shape[0]
     M = np.sqrt(N).round()
-    y = farthest_point_grid(X_train, M)
+    Y = farthest_point_grid(X_train, M)
+    P = density_estimation(X_train, Y)
+    clust = quick_shift(Y, P)
 
-    P = density_estimation(X_train, y)
-    clust = quick_shift(y, P)
-
-    print('number of clusters: ', len(np.unique(clust)))
-
-    plt.scatter(X_train[:, 0], X_train[:, 1], c='k', alpha=0.3, s=10)
+    print("number of clusters: ", len(np.unique(clust)))
+    plt.scatter(X_train[:, 0], X_train[:, 1], c="k", alpha=0.3, s=10)
     plt.scatter(y[:, 0], y[:, 1], c=clust, s=(P / 5))
     plt.show()
 
@@ -40,13 +39,13 @@ def main():
     gmm = build_gmm(y, P, clust)
     best = gmm.predict(X_test)
 
-    plt.scatter(X_train[:, 0], X_train[:, 1], c='k', alpha=0.3, s=10)
+    plt.scatter(X_train[:, 0], X_train[:, 1], c="k", alpha=0.3, s=10)
     plt.scatter(X_test[:, 0], X_test[:, 1], c=best, s=20)
     plt.show()
 
 
 def calc_distances(p0, points):
-    return ((p0 - points)**2).sum(axis=1)
+    return ((p0 - points) ** 2).sum(axis=1)
 
 
 def farthest_point_grid(x, m):
@@ -55,8 +54,7 @@ def farthest_point_grid(x, m):
     distances = calc_distances(farthest_pts[0], x)
     for i in range(1, int(m)):
         farthest_pts[i] = x[np.argmax(distances)]
-        distances = np.minimum(distances,
-                               calc_distances(farthest_pts[i], x))
+        distances = np.minimum(distances, calc_distances(farthest_pts[i], x))
     return farthest_pts
 
 
@@ -72,9 +70,10 @@ def density_estimation(x, y):
 
     sigma_j = np.array([delta_i[idx] for idx in min_dists])
 
-    prefactor = np.power(2 * np.pi * np.power(sigma_j, 2.), (- D / 2.))
-    gaussians = prefactor * (np.exp(-np.power(y_x_dists, 2.)
-                             / (2 * np.power(sigma_j, 2.))))
+    prefactor = np.power(2 * np.pi * np.power(sigma_j, 2.0), (-D / 2.0))
+    gaussians = prefactor * (
+        np.exp(-np.power(y_x_dists, 2.0) / (2 * np.power(sigma_j, 2.0)))
+    )
 
     pdf = np.sum(gaussians, axis=1)
 
@@ -88,7 +87,7 @@ def quick_shift(y, P):
     lamb = y_dists.min(axis=0).mean() * 1.3
 
     # create cluster id array to assign clusters
-    clusters = np.zeros_like(P, dtype='int')
+    clusters = np.zeros_like(P, dtype="int")
 
     def connect_to_neighbor(i):
         # find points with greater probability
@@ -132,12 +131,12 @@ class GaussianMixtureModel:
         Parameters:
         -----------
         X : array-like, shape (n_samples, n_features)
-        List of n_features-dimensional data points. Each row corresponds to a single data point.
+         List of n_features-dimensional data points. Each row corresponds to a single data point.
 
         Returns:
         --------
         labels : array, shape (n_samples,)
-        Component labels.
+         Component labels.
         """
         probs = self.predict_proba(X)
         return np.argmax(probs, axis=1)
@@ -149,12 +148,13 @@ class GaussianMixtureModel:
         Parameters:
         -----------
         X : array-like, shape (n_samples, n_features)
-        List of n_features-dimensional data points. Each row corresponds to a single data point.
+         List of n_features-dimensional data points. Each row corresponds to a single
+         data point.
 
         Returns:
         --------
         resp : array, shape (n_samples, n_components)
-        Returns the probability each Gaussian (state) in the model given each sample.
+         Returns the probability each Gaussian (state) in the model given each sample.
         """
         probs = np.zeros([X.shape[0], len(self.p)])
         for idx, weight in enumerate(self.p):
@@ -173,18 +173,21 @@ def build_gmm(y, P, clusters):
     # get position of each cluster center
     z_k = y[np.unique(clusters)].copy()
     # get pk for cluster
-    p_k = np.array([P[np.where(clusters == clust_id)].sum()/P.sum()
-                    for clust_id in np.unique(clusters)])
+    p_k = np.array(
+        [
+            P[np.where(clusters == clust_id)].sum() / P.sum()
+            for clust_id in np.unique(clusters)
+        ]
+    )
     # get sigma for each cluster
     sigma_k = np.zeros(shape=(len(z_k), y.shape[1], y.shape[1]))
     for idx, clust_id in enumerate(np.unique(clusters)):
         members = np.where(clusters == clust_id)
         distances = y[members] - z_k[idx]
-        sigma_k[idx] = np.cov(distances.T,
-                              aweights=(P[members] / total_P))
+        sigma_k[idx] = np.cov(distances.T, aweights=(P[members] / total_P))
 
     return GaussianMixtureModel(p_k, z_k, sigma_k)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
